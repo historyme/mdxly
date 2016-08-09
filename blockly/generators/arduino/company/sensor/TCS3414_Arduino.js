883456e7f03c1f618e08d1669f8768c1df999249
radiumray <radiumray9@gmail.com>
2016-08-09 09:56:17 +0800

    change colorDetector

--- blockly/generators/arduino/company/sensor/TCS3414_Arduino.js
+++ blockly/generators/arduino/company/sensor/TCS3414_Arduino.js
@@ -4,30 +4,19 @@ goog.provide('Blockly.Arduino.Microduino');
 
 goog.require('Blockly.Arduino');
 
-
-
 Blockly.Arduino.ColorDetPrepare_TCS3414 = function() {
 
   var TCS3414Include="#include <TCS3414Lib.h>\n";
   TCS3414Include+="#include <Wire.h>\n";
+  TCS3414Include+="#define colorSUM 3\n";
 
   Blockly.Arduino.definitions_['var_TCS3414Include'] = TCS3414Include;
 
   var TCS3414Var='';
-  TCS3414Var+='uint16_t rawRed,rawGreen,rawBlue,clr;\n';
   TCS3414Var+='uint8_t redColor,greenColor,blueColor;\n';
-  TCS3414Var+='uint16_t minFun;\n';
+  TCS3414Var+='uint16_t redValue, greenValue, blueValue, maxLight;\n';
   TCS3414Var+='TCS3414 tcs;\n';
 
-  TCS3414Var+='uint16_t minFunColorValue(uint16_t r, uint16_t g, uint16_t b) {\n';
-  TCS3414Var+='  if (r < minFun)\n';
-  TCS3414Var+='    minFun=r;\n';
-  TCS3414Var+='  if (g < minFun)\n';
-  TCS3414Var+='    minFun=g;\n';
-  TCS3414Var+='  if(b < minFun)\n';
-  TCS3414Var+='    minFun=b;\n';
-  TCS3414Var+='  return minFun;\n';
-  TCS3414Var+='}\n';
   Blockly.Arduino.definitions_['var_TCS3414defineVar'] = TCS3414Var;
 
 
@@ -40,25 +29,11 @@ Blockly.Arduino.ColorDetPrepare_TCS3414 = function() {
   Blockly.Arduino.setups_['setup_TCS3414Init'] = TCS3414Init;
 
   var code='\n';
-  code+='minFun=65535;\n';
-  code+='rawGreen-=8;\n';
-  code+='minFun=minFunColorValue(rawRed,rawGreen,rawBlue);\n';
-  code+='if(rawRed>minFun) {\n';
-  code+='  redColor=map((rawRed-minFun),0,rawRed,0,255);\n';
-  code+='} else {\n';
-  code+='  redColor=0;\n';
-  code+='}\n';
-  code+='if(rawGreen>minFun) {\n';
-  code+='  greenColor=map((rawGreen-minFun),0,rawGreen,0,255);\n';
-  code+='} else {\n';
-  code+='  greenColor=0;\n';
-  code+='}\n';
-  code+='if(rawBlue>minFun) {\n';
-  code+='  blueColor=map((rawBlue-minFun),0,rawBlue,0,255);\n';
-  code+='} else {\n';
-  code+='  blueColor=0;\n';
-  code+='}\n';
 
+  code+='tcs.getRGB(&redValue, &greenValue, &blueValue, &maxLight);\n';
+  code+='redColor = map(redValue, 0, maxLight * colorSUM, 0, 255);\n';
+  code+='greenColor = map(greenValue, 0, maxLight * colorSUM, 0, 255);\n';
+  code+='blueColor = map(blueValue, 0, maxLight * colorSUM, 0, 255);\n';
   return code;
 };
 
